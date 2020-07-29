a41d1a4610f94a199e781090d8673f52c7eb961a
Jacky20000 <44819410+Jacky20000@users.noreply.github.com>
2020-07-29 14:41:44 +0200

    doc comment
    one doc comment

--- src/logging.service.ts
+++ src/logging.service.ts
@@ -7,7 +7,10 @@ export class IssueLoggingService {
     issueCreatorUrl = "http://localhost:3500"
 
     constructor(private http: HttpService) {}
-
+   /**
+    * sending the log to the issue creator and displaying 'Log reported' on the console if it was successful and 
+    * 'ERROR when reporting log' if an error occured
+    */
     log(log: LogMessageFormat) {
         this.http.post(`${this.issueCreatorUrl}`, JSON.parse(JSON.stringify(log))).subscribe(
             res => console.log("Log reported"),
